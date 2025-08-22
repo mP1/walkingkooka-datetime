9390ecd05850d4691d4ab7871103d0c68d3f9bf8
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-08-22 17:40:43 +1000

    HasOptionalDateTimeSymbols

---
+++ src/main/java/walkingkooka/datetime/HasOptionalDateTimeSymbolsTesting.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.datetime;
+
+import walkingkooka.test.Testing;
+
+import java.util.Optional;
+
+public interface HasOptionalDateTimeSymbolsTesting extends Testing {
+
+    default void dateTimeSymbolsAndCheck(final HasDateTimeSymbols has) {
+        this.dateTimeSymbolsAndCheck(
+                has,
+                Optional.empty()
+        );
+    }
+
+    default void dateTimeSymbolsAndCheck(final HasDateTimeSymbols has,
+                                         final DateTimeSymbols expected) {
+        this.dateTimeSymbolsAndCheck(
+                has,
+                Optional.of(expected)
+        );
+    }
+
+    default void dateTimeSymbolsAndCheck(final HasDateTimeSymbols has,
+                                         final Optional<DateTimeSymbols> expected) {
+        this.checkEquals(
+                expected,
+                has.dateTimeSymbols(),
+                has::toString
+        );
+    }
+}
